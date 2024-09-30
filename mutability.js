25d5054ad46424e1f50b1d04a251a5e634a5cedc
krodgers <kaunda.rodgers@gmail.com>
2024-09-30 10:44:03 +0300

    mutability

---
+++ mutability.js
@@ -0,0 +1,13 @@
+const clone1 = {
+    name: person.name,
+    age : person.age,
+    country : person.country,
+};
+const clone2 = {
+    name : person.name,
+    age : person.age,
+    county : person.country,
+};
+const samePerson = person;
+person.age = person.age + 1
+person.country = 'FR'
