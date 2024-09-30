3f70c93527a05ad196c99902b35775243cbf0836
krodgers <kaunda.rodgers@gmail.com>
2024-09-30 10:16:28 +0300

    declarations

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "` \\ / \" '";
+const arr = Object.freeze([4, '2']);
+const obj = Object.freeze({
+    str : "string",
+    num : 5,
+    bool : true,
+    undef : undefined
+});
+const nested = Object.freeze({
+    arr : Object.freeze([4, '2']),
+    obj : Object.freeze({
+        str : "another string",
+        num : 3301,
+        bool : false
+    })
+});
