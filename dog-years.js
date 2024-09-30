a3ffc0b8301bf82c236637021d6514434278e812
krodgers <kaunda.rodgers@gmail.com>
2024-09-30 13:52:43 +0300

    dog-years

---
+++ dog-years.js
@@ -0,0 +1,23 @@
+const dogYears = (planet, dogAgeInSec) =>{
+    const orbits = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+    };
+
+    const secondsInEarthYear = 31557600;
+    
+    const ageInEarthYears = dogAgeInSec / secondsInEarthYear;
+        
+    const ageOnPlanet = ageInEarthYears / orbits[planet];
+        
+    const dogYears = ageOnPlanet * 7;
+        
+    return dogYears.toFixed(2);
+
+};
