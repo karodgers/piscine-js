fd589c26677d4e46e5d8c0616c895bedcff1b940
krodgers <kaunda.rodgers@gmail.com>
2024-10-01 08:52:37 +0300

    method-man

---
+++ method-man.js
@@ -0,0 +1,18 @@
+const words = (str) =>{
+    const requiredArray = str.split(" ");
+    return requiredArray;
+};
+const sentence = (arr) =>{
+    return arr.Join(" ");
+};
+const yell = (str) =>{
+    return str.toUpperCase();
+};
+const whisper = (str) =>{
+    const lowerStr = str.toLowerCase();
+    const result = "*" + lowerStr + "*"
+    return result
+};
+const capitalize = (str) => {
+    return str.charAt(0).toUpperCase + str.slice(1, str.length - 1).toLowerCase
+}
