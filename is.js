07476058b2000dcfabd2149a0c31096bee7124ab
krodgers <kaunda.rodgers@gmail.com>
2024-10-01 12:12:14 +0300

    is

--- is.js
+++ is.js
@@ -1,11 +1,44 @@
-num: value => typeof value === 'number' && !is.nan(value),
-nan: value => Number.isNaN(value),
-str: value => typeof value === 'string',
-bool: value => typeof value === 'boolean',
-undef: value => typeof value === 'undefined',
-def: value => typeof value !== 'undefined',
-arr: value => Array.isArray(value),
-obj: value => value !== null && typeof value === 'object',
-fun: value => typeof value === 'function',
-truthy: value => !!value,
-falsy: value => !value
+const num = (value) => {
+    return typeof value === 'number' && !isNaN(value);
+}
+
+const nan = (value) =>{
+    return Number.isNaN(value);
+}
+
+const str = (value) => {
+    return typeof value === 'string';
+}
+
+const bool = (value) => {
+    return typeof value === 'boolean';
+}
+
+const undef = (value) => {
+    return typeof value === 'undefined';
+}
+
+const def = (value)=> {
+    return typeof value !== 'undefined';
+}
+
+const arr = (value) => {
+    return Array.isArray(value);
+}
+
+const obj = (value) => {
+    return value !== null && typeof value === 'object';
+}
+
+const fun = (value) => {
+    return typeof value === 'function';
+}
+
+const truthy = (value) => {
+    return !!value;
+}
+
+const falsy = (value) => {
+    return !value;
+}
+
