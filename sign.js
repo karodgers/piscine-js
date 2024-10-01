a8b4dc6175cbcc05ea1e93ca55cf662a04ef6ff9
krodgers <kaunda.rodgers@gmail.com>
2024-10-01 10:07:51 +0300

    sign

--- sign.js
+++ sign.js
@@ -1,11 +1,11 @@
 const sign = (num) =>{
-    if (num = 0){
+    if (num === 0){
         return 0;
     }else if (num>0){
         return 1;
     }else if (num<0){
         return -1;
-    };
+    }
 };
 const sameSign = (num1, num2) =>{
     if ((num1<0 && num2<0) || (num1>0 && num2>0)){
