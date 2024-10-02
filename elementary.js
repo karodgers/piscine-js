db69eb6a842299842c9158eb441945f71317bff1
krodgers <kaunda.rodgers@gmail.com>
2024-10-02 14:22:11 +0300

    elementary

--- elementary.js
+++ elementary.js
@@ -1,5 +1,5 @@
 const multiply = (a, b) => {
-    if (b === 0) {
+    if (b === 0 || a===0) {
         return 0;
     }
     if (b < 0) {
@@ -9,11 +9,19 @@ const multiply = (a, b) => {
 };
 const divide = (a,b) => {
     if (b === 0) {
+        return undefined;
+    }
+    if (a === 0) {
         return 0;
     }
-    let quotient = 0
-
-    while (a>=b){
+    if (a < 0 && b < 0) {
+        return divide(-a, -b);
+    }
+    if (a < 0 || b < 0) {
+        return -divide(Math.abs(a), Math.abs(b));
+    }
+    let quotient = 0;
+    while (a >= b) {
         a = a - b;
         quotient++;
     }
@@ -22,10 +30,22 @@ const divide = (a,b) => {
 };
 const modulo =(a,b) => {
     if (b === 0) {
+        return undefined;
+    }
+    if (a === 0) {
         return 0;
     }
-    while (a>=b){
-        a = a - b;
+    if (a < 0 && b < 0){
+        return modulo(-a, -b);
+    }
+    if (a < 0) {
+        return -modulo(-a, b);
+    }
+    if (b < 0) {
+        return modulo(a, -b);
+    }
+    while (a >= b) {
+        a -= b;
     }
     return a;
 };
