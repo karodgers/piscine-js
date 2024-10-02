28935f4ca8756d980d1ff2fe0aa45a3f0653604d
krodgers <kaunda.rodgers@gmail.com>
2024-10-02 13:01:06 +0300

    elementary

--- elementary.js
+++ elementary.js
@@ -1,9 +1,9 @@
 const multiply = (a,b)=>{
-    if (a === 0 || b===0){
+    if (b===0){
         return 0;
     }
-    if (a<0||b<0){
-        return -multiply(-a, -b);
+    if (b<0){
+        return -multiply(a, -b);
     }
     return a + multiply(a, b-1);
 
