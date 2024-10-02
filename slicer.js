bb8293b8d0e45e55a9bcebcc4b148eee062b2221
krodgers <kaunda.rodgers@gmail.com>
2024-10-02 17:14:29 +0300

    slicer

--- slicer.js
+++ slicer.js
@@ -1,16 +1,38 @@
 const slice = (arrOrString, startingIndex, optionalEndingIndex) => {
     let result = [];
-    let end = optionalEndingIndex || arrOrString.length;
-    let resultIndex = 0; 
-
-    for (let i = startingIndex; i < end; i++) {
-        result[resultIndex] = arrOrString[i]; 
-        resultIndex++; 
+    let length = arrOrString.length;
+    let start = startingIndex;
+    let end = optionalEndingIndex;
+  
+    if (start < 0) {
+      start = length + start;
     }
-
+    if (start < 0) {
+      start = 0;
+    }
+  
+    if (end === undefined) {
+      end = length;
+    } else if (end < 0) {
+      end = length + end;
+    }
+    if (end > length) {
+      end = length;
+    }
+  
+    let resultIndex = 0;
+    for (let i = start; i < end; i++) {
+      result[resultIndex] = arrOrString[i];
+      resultIndex = resultIndex + 1;
+    }
+  
     if (typeof arrOrString === 'string') {
-        return result.join('');
+      let resultString = '';
+      for (let i = 0; i < result.length; i++) {
+        resultString = resultString + result[i];
+      }
+      return resultString;
     }
-
+  
     return result;
 };
