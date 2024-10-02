5a05d8d1f4317904e70876d0ef879620d1ed617a
krodgers <kaunda.rodgers@gmail.com>
2024-10-02 11:22:04 +0300

    collections

---
+++ collections.js
@@ -0,0 +1,54 @@
+const arrToSet = (arr) =>{
+    return new Set(arr);
+
+};
+const arrTostr = (arr) =>{
+    return arr.join("");
+};
+const setToArr = (set) =>{
+    return Array.from(set);
+};
+const setToStr = (set) =>{
+    return Array.from(set).join("");
+
+};
+const strToArr = (str) =>{
+    return str.split("");
+
+};
+const strToSet = (str) =>{
+    return new Set(str);
+
+};
+const mapToObj = (map) =>{
+    return Object.fromEntries(map);
+
+};
+const objToArr = (obj) =>{
+    return Object.values(obj);
+
+};
+const objToMap = (obj) =>{
+    return new Map(Object.entries(obj));
+
+};
+const arrToObj = (arr) =>{
+    return Object.assign({},arr);
+
+}
+const strToObj = (str) =>{
+    return Object.assign({}, str)
+
+}
+const supperTypeOf = (n) =>{
+
+}
+
+const superTypeOf = (value) => {
+  if (value === null) return 'null';
+  if (value === undefined) return 'undefined';
+  if (value instanceof Map) return 'Map';
+  if (value instanceof Set) return 'Set';
+  if (Array.isArray(value)) return 'Array';
+  return value.constructor.name;
+};
