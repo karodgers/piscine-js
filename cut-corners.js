fce2e3be4832a5b4bded4a3c60d6204bb2658d21
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 13:22:27 +0300

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -9,38 +9,47 @@ const round = (num) => {
 const ceil = (num) => {
     let intPart = floor(num);
     if (num > intPart) {
-        return intPart + 1;
+        return intPart + 1; 
     }
-    return intPart;
+    return intPart; 
 };
 
 const floor = (num) => {
     if (num >= 0) {
-        return trunc(num);
+        return trunc(num); 
     } else {
         let truncated = trunc(num);
         if (num < truncated) {
             return truncated - 1;
         }
-        return truncated;
+        return truncated; 
     }
 };
 
 const trunc = (num) => {
+    let result = num;
     if (num >= 0) {
-        let intPart = 0;
-        while (num >= 1) {
-            intPart += 1;
-            num -= 1;
+        while (result > 1) {
+            result -= 1;
         }
-        return intPart;
-    }
-    else {
-        let intPart = 0;  
-        while (num <= -1) { 
-            intPart -= 1;
-            num += 1;
+    } else {
+        while (result < -1) {
+            result += 1;
         }
-        return intPart;
     }
+    return num - result;
 };
+
+
+// const trunc = (num) => {
+//     if (num >= 1) {
+//         return 1 + trunc(num - 1);
+//     } else if (num < 0 && num > -1) {
+//         return 0;
+//     } else if (num < 0) {
+//         return -1 + trunc(num + 1);
+//     }
+//     return 0; 
+// };
+
+
