fd77ea214ab2504a48d32bce3ca1f17ce011e829
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 16:44:42 +0300

    nasa

--- nasa.js
+++ nasa.js
@@ -4,16 +4,19 @@ const nasa = (N) => {
     }
     let result = '';
     for (let i=0; i<=N; i++){
-        if (i%3 === 0 && i%5 === 0){
-            result = result + 'NASA';
-        }else if (i%3 === 0){
+
+        if (i%3 === 0){
             result = result + 'NA';
         }else if (i%5 === 0){
             result = result + 'SA';
+        }else if (i%3 === 0 && i%5 === 0){
+            result = result + 'NASA';
         }else{
             result = result + i.toString();
         }
         result = result + ' ';
     }
     return result.trim();
+
+
 }
