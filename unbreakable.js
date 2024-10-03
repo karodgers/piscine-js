2fb22e7eb9bc05a210b5683a8ac5502b0dfc757f
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 15:13:18 +0300

    unbreakable

---
+++ unbreakable.js
@@ -0,0 +1,48 @@
+const split = (str, sep) =>{
+    
+    if (typeof str !=='string'){
+        return;
+    }
+
+    const result = [];       
+    let currentSegment = ''; 
+   
+    for (let i = 0; i < string.length; i++) {
+
+        if (string.slice(i, i + sep.length) === sep) {
+            result.push(currentSegment); 
+            currentSegment = '';  
+            i += sep.length - 1; 
+        } else {
+            currentSegment += string[i]; 
+        }
+    }
+
+    if (currentSegment) {
+        result.push(currentSegment);
+    }
+
+    return result; 
+
+
+}
+
+const join = (arr, sep) => {
+
+    if (!Array.isArray(arr)) {
+       return;
+    }
+
+    let result = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+
+        if (i < arr.length - 1) {
+            result += sep;
+        }
+    }
+
+    return result; 
+
+}
