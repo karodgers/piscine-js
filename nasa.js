6f2dd0a60e2d6c32da4d6403803ae12547142091
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 16:41:55 +0300

    nasa

--- nasa.js
+++ nasa.js
@@ -1,19 +1,19 @@
-const nasa = (N, str) => {
+const nasa = (N) => {
     if (typeof N !== "number"){
         return ''
     }
-    let result = []
+    let result = '';
     for (let i=0; i<=N; i++){
         if (i%3 === 0 && i%5 === 0){
-            result = result + 'NASA'
+            result = result + 'NASA';
         }else if (i%3 === 0){
-            result = result + 'NA'
+            result = result + 'NA';
         }else if (i%5 === 0){
-            result = result + 'SA'
+            result = result + 'SA';
         }else{
-            result = result + i
+            result = result + i.toString();
         }
-        result.push(i.toString())
+        result = result + ' ';
     }
-    return result.join(' ')
+    return result.trim();
 }
