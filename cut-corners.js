348d06e1ed0ec7f7e7418224cfbfe9afa7388412
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 14:10:56 +0300

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,49 +1,65 @@
-const round = (nums) => {
-    if (nums >= 0) {
-        return floor(nums + 0.5);
+function findExponent(val) {
+    if (val >= -1 && val < 1){
+        return 0;
+    } 
+    let exp = 0;
+    let absVal = val < 0 ? -val : val;
+    while (absVal >= 2) {
+        absVal /= 2;
+        exp++;
+    }
+    return exp;
+}
+
+function approximateInteger(val, roundingFunc) {
+    if (val === 0 || (val > -1 && val < 1)) return 0;
+    
+    let sign = val < 0 ? -1 : 1;
+    let absVal = val < 0 ? -val : val;
+    
+    let base = 2 ** findExponent(absVal);
+    let result = base;
+    
+    while (result * sign <= val) {
+        result += 1;
+    }
+    
+    return roundingFunc(result, sign, val);
+}
+
+function truncateRounding(result, sign, val) {
+    return (result - 1) * sign;
+}
+
+function floorRounding(result, sign, val) {
+    return sign > 0 ? (result - 1) * sign : result * sign;
+}
+
+function ceilRounding(result, sign, val) {
+    return sign > 0 ? result * sign : (result - 1) * sign;
+}
+
+function roundRounding(result, sign, val) {
+    let diff = result * sign - val;
+    if (sign > 0) {
+        return diff > 0.5 ? (result - 1) * sign : result * sign;
     } else {
-        return ceil(nums - 0.5);
+        return diff < -0.5 ? result * sign : (result - 1) * sign;
     }
-};
+}
 
-const ceil = (nums) => {
-    let intPart = floor(nums);
-    if (nums > intPart) {
-        return intPart + 1; 
-    }
-    return intPart; 
-};
+function trunc(val) {
+    return approximateInteger(val, truncateRounding);
+}
 
-const floor = (nums) => {
-    if (nums >= 0) {
-        return trunc(nums); 
-    } else {
-        let truncated = trunc(nums);
-        if (nums < truncated) {
-            return truncated - 1;
-        }
-        return truncated; 
-    }
-};
+function floor(val) {
+    return approximateInteger(val, floorRounding);
+}
 
-const trunc = (num) => {
-    let integerPart = 0;
-    if (num >= 0) {
-        while (integerPart < num) {
-            integerPart++;
-        }
-        return integerPart === num ? integerPart : integerPart - 1;
-    } else {
-        integerPart = 0;
-        while (integerPart > num) {
-            integerPart--;
-        }
-        return integerPart === num ? integerPart : integerPart + 1;
-    }
-};
+function ceil(val) {
+    return approximateInteger(val, ceilRounding);
+}
 
-
-
-
-
-
+function round(val) {
+    return approximateInteger(val, roundRounding);
+}
