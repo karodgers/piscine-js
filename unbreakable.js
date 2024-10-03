7b4aa6732ea7f923d877c7855fe4f2b46612f84e
krodgers <kaunda.rodgers@gmail.com>
2024-10-03 15:40:17 +0300

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,33 +1,46 @@
 const split = (str, sep) => {
     if (typeof str !== 'string') {
-      return [];
+        return [];
     }
+
     const result = [];
-    let currentSegment = '';
-    const sepLength = sep.length;
+
+    let currentSegment = ''; 
+
+    const sepLength = sep.length; 
+   
     for (let i = 0; i < str.length; i++) {
-      if (str.slice(i, i + sepLength) === sep) {
-        result.push(currentSegment);
-        currentSegment = '';
-        i += sepLength - 1;
-      } else {
-        currentSegment += str[i];
-      }
+        if (str.slice(i, i + sepLength) === sep) {
+            result.push(currentSegment); 
+            currentSegment = '';  
+            i += sepLength - 1; 
+        } else {
+            currentSegment += str[i]; 
+        }
     }
+
     result.push(currentSegment);
-    return result;
-  }
-  
-  const join = (arr, sep) => {
+
+    return result; 
+}
+
+
+const join = (arr, sep) => {
+
     if (!Array.isArray(arr)) {
-      return '';
+       return;
     }
+
     let result = '';
+
     for (let i = 0; i < arr.length; i++) {
-      result += arr[i];
-      if (i < arr.length - 1) {
-        result += sep;
-      }
+        result += arr[i];
+
+        if (i < arr.length - 1) {
+            result += sep;
+        }
     }
-    return result;
-  }
+
+    return result; 
+
+}
