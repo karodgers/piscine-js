3ac224cf454c69c699b1bcb7b2dac81b76cb0c29
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 10:07:13 +0300

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,28 +1,26 @@
-const pyramid = (character, depth) => {
-    let maxWidth = 2 * depth - 1;
+const pyramid = (str, num) => {
+    let maxWidth = (2 * num - 1) * str.length;
     let result = '';
 
-    for (let i = 0; i < depth; i++) {
+    for (let i = 0; i < num; i++) {
         let row = '';
-        let charCount = 2 * i + 1;
+        let charCount = (2 * i + 1) * str.length;
         let spaceCount = (maxWidth - charCount) / 2;
 
         for (let j = 0; j < spaceCount; j++) {
             row += ' ';
         }
 
-        for (let k = 0; k < charCount; k++) {
-            row += character;
+        for (let k = 0; k < charCount / str.length; k++) {
+            row += str;
         }
-        
+
         result += row;
 
-        if (i < depth - 1) {
+        if (i < num - 1) {
             result += '\n';
         }
     }
-    
+
     return result;
 }
-
-
