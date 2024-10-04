28e879ff9d280fac9fda7ceb02fb8acf0fe478b9
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 08:58:10 +0300

    pyramid

--- cut-corners.js
+++ cut-corners.js
@@ -1,61 +1,85 @@
 const round = (num) => {
-    if (num >= 0) {
-      return floor(num + 0.5);
-    } else {
-      return ceil(num - 0.5);
-    }
+  if (num >= 0) {
+
+    return floor(num + 0.5);
+
+  } else {
+    
+    return ceil(num - 0.5);
+  }
 };
   
 const ceil = (num) => {
-    let intPart = floor(num);
-    if (num > intPart) {
-      return intPart + 1;
-    }
-    return intPart;
+  let intPart = floor(num);
+
+  if (num > intPart) {
+
+    return intPart + 1;
+  }
+
+  return intPart;
 };
   
 const floor = (num) => {
-    if (num >= 0) {
-      return trunc(num);
-    } else {
-      let truncated = trunc(num);
-      if (num < truncated) {
-        return truncated - 1;
-      }
-      return truncated;
+  if (num >= 0) {
+
+    return trunc(num);
+
+  } else {
+
+    let truncated = trunc(num);
+
+    if (num < truncated) {
+
+      return truncated - 1;
+
     }
+    return truncated;
+  }
 };
   
 const trunc = (num) => {
-    if (num >= 0) {
-      let result = 0;
-      let step = 1;
-      while (result + step <= num) {
-        result += step;
-        step += step;
-      }
-      step = step / 2;
-      while (step >= 1) {
-        if (result + step <= num) {
-          result += step;
-        }
-        step = step / 2;
-      }
-      return result;
-    } else {
-      let result = 0;
-      let step = 1;
-      while (result - step >= num) {
-        result -= step;
-        step += step;
-      }
-      step = step / 2;
-      while (step >= 1) {
-        if (result - step >= num) {
-          result -= step;
-        }
-        step = step / 2;
-      }
-      return result;
+  
+  if (num >= 0) {
+    let result = 0;
+    let step = 1;
+
+    while (result + step <= num) {
+      result += step;
+      step += step;
     }
+
+    step = step / 2;
+
+    while (step >= 1) {
+      if (result + step <= num) {
+        result += step;
+      }
+      step = step / 2;
+    }
+
+    return result;
+
+  } else {
+
+    let result = 0;
+    let step = 1;
+    while (result - step >= num) {
+      result -= step;
+      step += step;
+    }
+
+    step = step / 2;
+
+    while (step >= 1) {
+
+      if (result - step >= num) {
+        result -= step;
+      }
+
+      step = step / 2;
+    }
+
+    return result;
+  }
 };
---
+++ pyramid.js
@@ -0,0 +1,32 @@
+const pyramid = (str, num) =>{
+    
+    if (typeof str!= 'string'){
+        return ''
+    }
+   
+    if (typeof num!='number'){
+        return ''
+    }
+
+    let result = '';
+
+    for (let i = 1; i <= num; i++) {
+        let line = '';
+
+        for (let j = 0; j < num - i; j++) {
+            line += ' ';
+        }
+
+        for (let j = 0; j < 2 * i - 1; j++) {
+            line += str;
+        }
+
+        result += line;
+
+        if (i < num) {
+            result += '\n';
+        }
+    }
+
+    return result;
+}
