c53af08a9bec17474f2e5a2e47fcb08dde0087da
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 17:52:16 +0300

    letter space number

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,18 +2,17 @@ const letterSpaceNumber = (str) => {
     let result = [];
     let i = 0;
 
-    while (i < str.length - 2) { 
-        let threeChars = str.slice(i, i + 3);
-        
-        if (threeChars.match(/^\w\s\d$/)) {
-            let nextChar = str[i + 3];
-            let isNextCharLetter = nextChar.match(/[a-zA-Z]/) !== null;
-            
-            if (i + 3 >= str.length || !isNextCharLetter) {
-                result.push(threeChars);
+    while (i < str.length - 1) { 
+
+        if (/[a-zA-Z]/.test(str[i])) {
+
+            if (str[i + 1] === ' ' && /\d/.test(str[i + 2]) && str[i + 2] < '10') {
+
+                if (i + 3 >= str.length || !/[a-zA-Z]/.test(str[i + 3])) {
+                    result.push(str[i] + ' ' + str[i + 2]);
+                }
             }
         }
-        
         i++;
     }
 
