bb94f8094a21a8cdf9d634cfaa2567d02570cac5
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 08:36:29 +0300

    triangle

--- triangle.js
+++ triangle.js
@@ -6,8 +6,13 @@ const triangle = (str, num) =>{
         return '';
     }
     let result = '';
-    for(let i = 0; i<=num; i++){
-        result = result + str + '\n';
+    for (let i = 1; i <= num; i++) { 
+        let line = '';
+        for (let j = 0; j < i; j++) { 
+            line = line + str;
+        }
+        result = result + line + '\n'; 
     }
+
     return result;
 }
