a8bb6514345af05370d4ceaf9049b6a2f8360961
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 16:44:54 +0300

    sum

--- sums.js
+++ sums.js
@@ -1,29 +1,25 @@
 const sums = (num) => {
-    if (num === 0) {
+    if (num === 0 || num === 1) {
         return [];
     }
-
-    var partitions = [];
-    var current = [];
-    var i, j;
-
+    
+    const partitions = new Set();
+    const current = [];
+    
     const findPartitions = (target, max) => {
         if (target === 0) {
-            var partition = [];
-            for (j = 0; j < current.length; j++) {
-                partition[j] = current[j];
-            }
-            partitions.push(partition);
+            partitions.add(current.slice().sort((a, b) => a - b).join(','));
             return;
         }
-
-        for (i = 1; i <= max && i <= target; i++) {
+        
+        for (let i = 1; i <= max && i <= target; i++) {
             current.push(i);
             findPartitions(target - i, i);
             current.pop();
         }
     };
-
+    
     findPartitions(num, num);
-    return partitions;
+    
+    return Array.from(partitions).map(partition => partition.split(',').map(Number));
 };
