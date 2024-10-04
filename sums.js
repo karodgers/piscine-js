55cc1b6aef63656294d5b0408d4be9e32c912477
krodgers <kaunda.rodgers@gmail.com>
2024-10-04 14:58:39 +0300

    partitions

---
+++ sums.js
@@ -0,0 +1,34 @@
+const sums = (num) =>{
+    var partitions = [];
+    var current = [];
+    var i, j;
+  
+    const findPartitions = (target, max) =>{
+      if (target === 0) {
+
+        var partition = [];
+
+        for (j = 0; j < current.length; j++) {
+          partition[j] = current[j];
+        }
+
+        partitions[partitions.length] = partition;
+        
+        return;
+      }
+      
+      for (i = 1; i <= max && i <= target; i++) {
+
+        current[current.length] = i;
+
+        findPartitions(target - i, i);
+        
+        current.length = current.length - 1;
+      }
+    }
+    
+    findPartitions(num, num);
+    return partitions;
+
+}
+
