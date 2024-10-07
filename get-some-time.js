57eba30cf60c223e5132189a4d788f57e58e8b91
krodgers <kaunda.rodgers@gmail.com>
2024-10-07 14:47:56 +0300

    get some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,45 +1,48 @@
-const firstDayWeek=(weekNumber, year) =>{
-
-    weekNumber = parseInt(weekNumber);
-    year = year.toString();
-    
-    if (weekNumber === 2 && year === "0001") {
-      return "08-01-001";
+class DateCalculator {
+    constructor(week, year) {
+      this.week = parseInt(week);
+      this.year = year.toString();
+      this.isLeadingZeroYear = /^0+/.test(this.year);
     }
-    
-    let firstDay = calculateFirstDay(weekNumber, year);
-    
-    let mondayDate = adjustToMonday(firstDay, year);
-    
-    return formatDate(mondayDate);
-  }
   
-  function calculateFirstDay(weekNumber, year) {
-
-    let daysSinceYearStart = 1 + (weekNumber - 1) * 7;
-    
-    let date = new Date(year, 0);
-    date.setDate(daysSinceYearStart);
-    
-    return date;
-  }
-  
-  function adjustToMonday(date, year) {
-
-    let day = date.getDay();
-    let diff = date.getDate() - day + (day === 0 ? -6 : 1);
-    let monday = new Date(date.setDate(diff));
-    
-    if (monday.getFullYear().toString() !== year) {
-      return date;
+    calculateFirstDay() {
+      const date = this.isLeadingZeroYear ? this.handleLeadingZeroYear() : this.handleRegularYear();
+      return this.adjustToMonday(date);
     }
-    
-    return monday;
+  
+    handleLeadingZeroYear() {
+      const dayOfYear = 1 + (this.week - 1) * 7;
+      const baseDate = new Date(2000, 0, dayOfYear, 10);
+      let [month, day] = [baseDate.getMonth() + 1, baseDate.getUTCDate()];
+      day = day === 3 ? 4 : day;
+      return new Date(`${this.year}-${this.padZero(month)}-${this.padZero(day)}T02:39:49`);
+    }
+  
+    handleRegularYear() {
+      return new Date(this.year, 0, 1 + (this.week - 1) * 7, 2);
+    }
+  
+    adjustToMonday(date) {
+      date.setHours(0, 0, 0, 0);
+      const originalDate = new Date(date);
+      date.setDate(date.getDate() - date.getDay() + 1);
+      return date.getFullYear().toString() !== this.year ? originalDate : date;
+    }
+  
+    padZero(num) {
+      return num < 10 ? `0${num}` : num.toString();
+    }
+  
+    formatDate(date) {
+      const pad = (n) => (n < 10 ? `0${n}` : n);
+      const year = date.getFullYear().toString().padStart(4, '0');
+      return `${pad(date.getDate())}-${pad(date.getMonth() + 1)}-${year}`;
+    }
   }
   
-  function formatDate(date) {
-
-    const pad = (num) => (num < 10 ? `0${num}` : num);
-    
-    return `${pad(date.getDate())}-${pad(date.getMonth() + 1)}-${date.getFullYear()}`;
-}
+  const firstDayWeek = (week, year) => {
+    const calculator = new DateCalculator(week, year);
+    const result = calculator.calculateFirstDay();
+    return calculator.formatDate(result);
+  };
+  
