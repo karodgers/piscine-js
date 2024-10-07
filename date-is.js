6ef8bb3349c960a91404e9fc0e3fe0906e99f79e
krodgers <kaunda.rodgers@gmail.com>
2024-10-07 11:35:14 +0300

    date is

--- date-is.js
+++ date-is.js
@@ -1,8 +1,16 @@
 const isValid = (date) =>{
 
-    date = new Date(date);
+    if (typeof date === 'string' || typeof date === 'number') {
+       
+        date = new Date(date);
+
+    } else {
+        
+        date = new Date(date);
+    }
     
     return date instanceof Date && !isNaN(date.getTime());
+
 };
 
 const isAfter = (date1, date2) => {
