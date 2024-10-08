333bfa452c791365b34bbcf87f64e4fbbb4c7a0f
krodgers <kaunda.rodgers@gmail.com>
2024-10-08 08:27:47 +0300

    count leap year

--- count-leap-years.js
+++ count-leap-years.js
@@ -3,7 +3,7 @@ const countLeapYears = (date) => {
    
     let count = 0;
 
-    for (let i = 1; i <= year; i++) {
+    for (let i = 1; i < year; i++) {
 
         if ((i % 4 === 0 && i % 100 !== 0) || (i % 400 === 0)) {
             count++;
