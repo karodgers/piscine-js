35cd71ca8b6f5c979ddaf41fce1ff8ae21d47b4c
krodgers <kaunda.rodgers@gmail.com>
2024-10-08 08:43:13 +0300

    day of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,10 +1,10 @@
 const dayOfTheYear = (date) => {
 
     var startOfYear = new Date(date.getFullYear(), 0, 1);
-    
+
     var millisecondsInADay = 86400000; 
 
-    var difference = (date - startOfYear) / millisecondsInADay;
+    var difference = (date.getTime() - startOfYear.getTime()) / millisecondsInADay;
 
     return Math.floor(difference) + 1; 
 }
