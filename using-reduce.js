f95c7d1522a6b3f001d5dd8be51afc574eefc14c
krodgers <kaunda.rodgers@gmail.com>
2024-10-08 14:35:01 +0300

    using reduce

---
+++ using-reduce.js
@@ -0,0 +1,40 @@
+const adder = (arrNums) =>{
+
+    const sumOfNums = arrNums.reduce(function(sum, currentNumber){
+
+        return sum + currentNumber
+
+    }, arrNums[0]);
+}
+
+const sumOrMul = (arrNums) => {
+
+    if (arrNums.length === 0){
+        
+        return 0;
+    }
+    const result = arrNums.reduce((result, currentNumber) => {
+
+        if (currentNumber % 2 === 0) {
+
+            return result * currentNumber;
+
+        } else {
+
+            return result + currentNumber;
+        }
+    }, 0);
+
+  return result;
+};
+
+const funcExcec = (arrFuncs, initialVal) =>{
+
+    result = arrFuncs.reduce((acc, func) => {
+
+        return func(acc);
+
+    }, initialVal);
+
+    return result;
+}
