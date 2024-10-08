53f1ffe0cb53a1ef97770e4e8651590ff11c4cb0
krodgers <kaunda.rodgers@gmail.com>
2024-10-08 09:31:36 +0300

    sunny sunday

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,12 +1,15 @@
 const sunnySunday = (date) => {
 
     const inputDate = new Date(date);
-    
+
     const dayOfWeek = inputDate.getUTCDay();
 
-    const adjustedDay = (dayOfWeek === 0) ? 6 : dayOfWeek - 1;
+
+    if (dayOfWeek === 0) {
+        return 'Saturday';
+    }
 
     const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    return weekdays[adjustedDay];
-}
+    return weekdays[dayOfWeek - 1];
+};
