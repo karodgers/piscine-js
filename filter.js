2ae672583a1789753c60a5afb59f214d1b645e72
krodgers <kaunda.rodgers@gmail.com>
2024-10-09 16:58:20 +0300

    filter

---
+++ filter.js
@@ -0,0 +1,57 @@
+function filter(arr, func) {
+
+    const result = []; 
+    
+    for (let i = 0; i < arr.length; i++) { 
+
+        if (func(arr[i], i, arr)) { 
+
+            result.push(arr[i]); 
+        }
+    }
+    
+    return result; 
+};
+
+
+const reject = (arr, predicate) => {
+    
+    const result = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+
+      const item = arr[i];
+  
+      if (!predicate(item)) {
+
+        result.push(item);
+      }
+    }
+  
+    return result;
+};
+
+const partition = (array, predicate) => {
+    
+    const truthyGroup = [];
+
+    const falsyGroup = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i];
+  
+      if (predicate(item)) {
+
+        truthyGroup.push(item);
+
+      } else {
+
+        falsyGroup.push(item);
+
+      }
+    }
+  
+    return [truthyGroup, falsyGroup];
+};
+
+
