06c72f844b5442b512ed2c468e211d719c6ca621
karodgers <kaunda.rodgers@gmail.com>
2024-10-10 09:21:58 +0300

    has city

---
+++ has-city.js
@@ -0,0 +1,14 @@
+const hasCity = (country, cities) => {
+
+    return function(city) {
+
+        if (cities.includes(city)) {
+
+            return `${city} is a city from ${country}.`;
+
+        } else {
+            
+            return `${city} is not a city from ${country}.`;
+        }
+    };
+}
