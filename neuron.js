2b4094c06a08d5511da2fc343d2ce6ee5ac3b3e1
krodgers <kaunda.rodgers@gmail.com>
2024-10-16 09:16:07 +0300

    neuron

--- neuron.js
+++ neuron.js
@@ -1,38 +1,41 @@
 const neuron = (data) => {
 
-    var output = { questions: {}, orders: {} };
-  
-    for (var i = 0; i < data.length; i++) {
+  if (data.length === 0) {
 
-      var parts = data[i].split(' - ');
-      var typePart = parts[0].split(': ');
-      var responsePart = parts[1].split(': ');
-  
-      var category = typePart[0].toLowerCase();
-      var description = typePart[1];
-      var response = responsePart[1];
-  
-      var key = description.replace(/\s+/g, '_').toLowerCase().replace('?', '');
-  
-      if (category === 'questions') {
+    return {};
 
-        if (!output.questions[key]) {
+  }
 
-          output.questions[key] = { question: description, responses: [] };
-        }
+  var output = { questions: {}, orders: {} };
 
-        output.questions[key].responses.push(response);
+  for (var i = 0; i < data.length; i++) {
+
+    var parts = data[i].split(' - ');
+    var typePart = parts[0].split(': ');
+    var responsePart = parts[1].split(': ');
+
+    var category = typePart[0].toLowerCase();
+    var description = typePart[1];
+    var response = responsePart[1];
+
+    var key = description.replace(/\s+/g, '_').toLowerCase().replace('?', '');
+
+    if (category === 'questions') {
+
+      if (!output.questions[key]) {
+        output.questions[key] = { question: description, responses: [] };
       }
-  
-      if (category === 'orders') {
-
-        if (!output.orders[key]) {k
-
-          output.orders[key] = { order: description, responses: [] };
-        }
-        output.orders[key].responses.push(response);
-      }
+      output.questions[key].responses.push(response);
     }
-  
-    return output;
+
+    if (category === 'orders') {
+
+      if (!output.orders[key]) {
+        output.orders[key] = { order: description, responses: [] };
+      }
+      output.orders[key].responses.push(response);
+    }
+  }
+
+  return output;
 }
