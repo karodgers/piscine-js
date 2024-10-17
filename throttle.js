3de5ce077d9f22527ba97fa00d022439eac72a37
krodgers <kaunda.rodgers@gmail.com>
2024-10-17 14:36:57 +0300

    throttle

--- throttle.js
+++ throttle.js
@@ -1,4 +1,3 @@
-
 const throttle = (mainFunction, delay) => {
 
     let timerFlag = null; 
@@ -12,79 +11,41 @@ const throttle = (mainFunction, delay) => {
       }
     };
 }
-const opThrottle = (func, wait, option = { leading: true, trailing: true }) =>{
-    let waiting = false;
-    let lastArgs = null;
-    let lastResult = undefined;
 
-    return function wrapper(...args) {
-        if (!waiting) {
-            waiting = true;
-            if (option.leading) {
-                lastResult = func.apply(this, args);
-            } else {
-                lastArgs = args;
-            }
-            setTimeout(() => {
-                if (option.trailing && lastArgs) {
-                    lastResult = func.apply(this, lastArgs);
-                    lastArgs = null;
-                }
-                waiting = false;
-            }, wait);
-        } else {
-            lastArgs = args;
+
+const opThrottle = (func, wait = 0, options = {leading: false, trailing: true}) =>{
+    
+    let lastArgs, lastContext;
+    let timer = null;
+    let result;
+
+    const invoke = () => {
+        result = func.apply(lastContext, lastArgs);
+        lastArgs = lastContext = null;
+    };
+
+    return function(...args) {
+        lastArgs = args;
+        lastContext = this;
+
+        const isLeading = options.leading && !timer;
+
+        if (isLeading) {
+            invoke();
         }
 
-        return option.leading || option.trailing ? lastResult : undefined;
+        if (!timer) {
+            timer = setTimeout(() => {
+                timer = null;
+                if (options.trailing) {
+                    invoke();
+                }
+            }, wait);
+        }
+
+        return result;
     };
 }
-
-
-
-// const opThrottle = (func, wait = 0, options = { leading: false, trailing: true }) => {
-//     let lastArgs, lastContext;
-//     let timer = null;
-//     let result;
-//     let invoked = false;
-
-//     const invoke = () => {
-//         result = func.apply(lastContext, lastArgs);
-//         lastArgs = lastContext = null;
-//     };
-
-//     return function (...args) {
-//         lastArgs = args;
-//         lastContext = this;
-
-//         const isLeading = options.leading && !invoked;
-
-//         if (isLeading) {
-//             invoke();
-//             invoked = true;
-//             return result; 
-//         }
-
-//         if (!timer) {
-//             timer = setTimeout(() => {
-//                 timer = null;
-//                 if (options.trailing && invoked) {
-//                     invoke();
-//                 }
-//                 invoked = false;
-//             }, wait);
-//         }
-
-//         if (options.trailing && !isLeading) {
-//             invoked = true;
-//         }
-
-//         return result;  
-//     };
-// }
-
-
-
 
 
 
