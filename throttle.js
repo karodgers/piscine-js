8db08dfc966297e86dbc79a520f2793f526fd98a
krodgers <kaunda.rodgers@gmail.com>
2024-10-17 12:55:10 +0300

    throttle

--- throttle.js
+++ throttle.js
@@ -1,49 +1,39 @@
-const throttle = (func, wait) => {
-    let lastTime = 0;
-
-    return function(...args) {
+const createThrottle = (wait = 0) => {
+    let lastInvoked = 0;
+    let timeoutId = null;
+  
+    return (func, options = { leading: true, trailing: true }) => {
+      return (...args) => {
+        const { leading, trailing } = options;
         const now = Date.now();
-        if (now - lastTime >= wait) {
-            lastTime = now;
-            return func(...args);
-        }
-    };
-}
-function opThrottle(func, wait, options = { leading: true, trailing: true }) {
-   
-    let lastTime = 0;
-    let timeout;
-    let lastArgs;
-    let leadingInvoked = false;
-
-    return function(...args) {
-        const now = Date.now();
-        const isFirstCall = lastTime === 0;
-        const remainingTime = isFirstCall ? 0 : wait - (now - lastTime);
-
-        lastArgs = args;
-
-        if (remainingTime <= 0 || isFirstCall) {
-            if (timeout) {
-                clearTimeout(timeout);
-                timeout = null;
+        const remainingTime = wait - (now - lastInvoked);
+  
+        const invokeFunction = () => {
+          lastInvoked = now;
+          func(...args);
+        };
+  
+        const scheduleTrailing = () => {
+          timeoutId = setTimeout(() => {
+            const shouldInvoke = Date.now() - lastInvoked >= wait;
+            if (shouldInvoke && trailing) {
+              invokeFunction();
             }
-
-            if (options.leading && !leadingInvoked) {
-                leadingInvoked = true;
-                lastTime = now;
-                return func(...args);
-            }
-
-            lastTime = now;
-            return func(...args);
-        } else if (options.trailing && !timeout) {
-            timeout = setTimeout(() => {
-                timeout = null;
-                if (options.trailing && lastArgs) {
-                    func(...lastArgs);
-                }
-            }, remainingTime);
+          }, wait);
+        };
+  
+        if (remainingTime <= 0) {
+          clearTimeout(timeoutId);
+          if (leading) {
+            invokeFunction();
+          }
+          scheduleTrailing();
+        } else if (!timeoutId && trailing) {
+          scheduleTrailing();
         }
+      };
     };
-}
+};
+  
+const throttle = createThrottle();
+const opThrottle = createThrottle();
