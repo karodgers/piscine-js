c652eca8b7f0a6a4768ef4828b104d47385f8525
krodgers <kaunda.rodgers@gmail.com>
2024-10-17 13:02:04 +0300

    throttle

--- throttle.js
+++ throttle.js
@@ -1,39 +1,76 @@
-const createThrottle = (wait = 0) => {
-    let lastInvoked = 0;
-    let timeoutId = null;
+const throttle = (func, wait) => {
+    
+    let lastTime = 0;
+    let timeout;
   
-    return (func, options = { leading: true, trailing: true }) => {
-      return (...args) => {
-        const { leading, trailing } = options;
-        const now = Date.now();
-        const remainingTime = wait - (now - lastInvoked);
-  
-        const invokeFunction = () => {
-          lastInvoked = now;
-          func(...args);
-        };
-  
-        const scheduleTrailing = () => {
-          timeoutId = setTimeout(() => {
-            const shouldInvoke = Date.now() - lastInvoked >= wait;
-            if (shouldInvoke && trailing) {
-              invokeFunction();
-            }
-          }, wait);
-        };
-  
-        if (remainingTime <= 0) {
-          clearTimeout(timeoutId);
-          if (leading) {
-            invokeFunction();
-          }
-          scheduleTrailing();
-        } else if (!timeoutId && trailing) {
-          scheduleTrailing();
-        }
-      };
+    return function(...args) {
+
+      const now = Date.now();
+
+      if (!lastTime || now - lastTime >= wait) {
+
+        lastTime = now;
+
+        func(...args);
+
+      } else {
+
+        clearTimeout(timeout); 
+
+        timeout = setTimeout(() => {
+
+          lastTime = Date.now();
+
+          func(...args); 
+
+        }, wait - (now - lastTime));
+      }
     };
-};
+}
+
+
+const opThrottle = (func, wait = 0, options = {leading: false, trailing: true}) =>{
+    
+    let lastArgs, lastContext;
+    let timer = null;
+    let result;
+
+    const invoke = () => {
+        result = func.apply(lastContext, lastArgs);
+        lastArgs = lastContext = null;
+    };
+
+    return function(...args) {
+        lastArgs = args;
+        lastContext = this;
+
+        const isLeading = options.leading && !timer;
+
+        if (isLeading) {
+            invoke();
+        }
+
+        if (!timer) {
+            timer = setTimeout(() => {
+                timer = null;
+                if (options.trailing) {
+                    invoke();
+                }
+            }, wait);
+        }
+
+        return result;
+    };
+}
+
+
+
+
+
+
   
-const throttle = createThrottle();
-const opThrottle = createThrottle();
+
+  
+  
+  
+  
