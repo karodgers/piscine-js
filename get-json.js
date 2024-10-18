31f0f2bc40324ac760e1ebd973015be020b6e5ac
krodgers <kaunda.rodgers@gmail.com>
2024-10-18 16:26:18 +0300

    get json

--- get-json.js
+++ get-json.js
@@ -1,27 +1,27 @@
 const getJSON = async (path, params = {}) => {
     
-    const url = new URL(path);
-    
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    const queryString = new URLSearchParams(params).toString();
+
+    const url = queryString ? `${path}?${queryString}` : path;
   
     try {
 
       const response = await fetch(url);
   
       if (!response.ok) {
-        throw new Error(response.statusText); 
+        throw new Error(response.statusText);
       }
   
       const result = await response.json();
   
       if (result.error) {
-        throw new Error(result.error); 
+        throw new Error(result.error);
       }
   
       return result.data;
   
     } catch (error) {
-
+        
       throw new Error(error.message);
     }
 }
