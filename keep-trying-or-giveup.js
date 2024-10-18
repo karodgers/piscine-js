07bcdce47bcc0a0baf7c2c0760ba3e9dfecb02a5
karodgers <kaunda.rodgers@gmail.com>
2024-10-18 11:55:36 +0300

    keep trying or give up

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,48 @@
+const retry = (count, callback) =>{
+
+    return async function(...args) {
+      let attempts = 0; 
+      
+      while (attempts <= count) {
+
+        try {
+
+          return await callback(...args); 
+          
+        } catch (error) {
+
+          attempts++; 
+
+          if (attempts > count) {
+
+            throw new Error('Too many errors'); 
+          }
+        }
+      }
+    };
+}
+
+  
+const timeout = (delay, callback) => {
+
+    return async function(...args) {
+
+      return new Promise((resolve, reject) => {
+
+        const timer = setTimeout(() => {
+          reject(new Error('timeout')); 
+        }, delay);
+  
+        callback(...args)
+          .then(result => {
+            clearTimeout(timer); 
+            resolve(result);     
+          })
+          .catch(error => {
+            clearTimeout(timer); 
+            reject(error);      
+        });
+      });
+    };
+}
+  
