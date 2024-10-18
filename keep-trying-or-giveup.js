a765e836c7aab188f89aa9b5fc4a5b710dcc53c5
krodgers <kaunda.rodgers@gmail.com>
2024-10-18 15:37:14 +0300

    keep trying or give up

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,48 +1,37 @@
-const retry = (count, callback) =>{
+const retry = (count, callback) => {
+  return async function (...args) {
+    let attempts = 0;
 
-    return async function(...args) {
-      let attempts = 0; 
-      
-      while (attempts <= count) {
+    while (attempts <= count) {
+      try {
+        return await callback(...args);
+      } catch (error) {
+        attempts++;
 
-        try {
-
-          return await callback(...args); 
-          
-        } catch (error) {
-
-          attempts++; 
-
-          if (attempts > count) {
-
-            throw new Error('Too many errors'); 
-          }
+        if (attempts > count) {
+          throw new Error('Too many errors');
         }
       }
-    };
-}
+    }
+  };
+};
 
-  
 const timeout = (delay, callback) => {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(new Error('timeout'));
+      }, delay);
 
-    return async function(...args) {
-
-      return new Promise((resolve, reject) => {
-
-        const timer = setTimeout(() => {
-          reject(new Error('timeout')); 
-        }, delay);
-  
-        callback(...args)
-          .then(result => {
-            clearTimeout(timer); 
-            resolve(result);     
-          })
-          .catch(error => {
-            clearTimeout(timer); 
-            reject(error);      
+      callback(...args)
+        .then(result => {
+          clearTimeout(timer);
+          resolve(result);
+        })
+        .catch(error => {
+          clearTimeout(timer);
+          reject(error);
         });
-      });
-    };
-}
-  
+    });
+  };
+};
