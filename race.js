f2d3a7ea5211912445b43aa6b2a546273cad02e4
karodgers <kaunda.rodgers@gmail.com>
2024-10-19 12:26:50 +0300

    race

--- race.js
+++ race.js
@@ -1,11 +1,7 @@
 function race(promises) {
-
     return new Promise((resolve, reject) => {
-
       for (let i = 0; i < promises.length; i++) {
-
         let p = promises[i];
-
         if (p instanceof Promise) {
           p.then(resolve).catch(reject);
         } else {
@@ -13,10 +9,11 @@ function race(promises) {
         }
       }
     });
-  }
-  
-  function some(promises, count) {
+}
 
+
+
+function some(promises, count) {
     if (count === 0) {
       return new Promise((resolve) => resolve([]));
     }
@@ -26,39 +23,32 @@ function race(promises) {
     }
   
     return new Promise((resolve, reject) => {
-
-      let resolvedValues = new Array(count);
+      const resolvedValues = new Array(count);
       let resolvedCount = 0;
-      let indexMap = new Map();
   
       for (let i = 0; i < promises.length; i++) {
-
         let p = promises[i];
   
-        indexMap.set(i, resolvedCount);
-
         if (p instanceof Promise) {
-
           p.then((value) => {
-            resolvedValues[indexMap.get(i)] = value;
-            resolvedCount++;
-
+            if (resolvedCount < count) {
+              resolvedValues[resolvedCount] = value;
+              resolvedCount++;
+            }
             if (resolvedCount === count) {
               resolve(resolvedValues);
             }
           }).catch(reject);
-
         } else {
-
-          resolvedValues[indexMap.get(i)] = p;
-          resolvedCount++;
-          
+          if (resolvedCount < count) {
+            resolvedValues[resolvedCount] = p;
+            resolvedCount++;
+          }
           if (resolvedCount === count) {
             resolve(resolvedValues);
           }
         }
       }
     });
-  }
-  
+}
   
