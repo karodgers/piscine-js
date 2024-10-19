21a2c5935efb02d2d598c64eabc7b9c56708f05c
karodgers <kaunda.rodgers@gmail.com>
2024-10-19 12:31:02 +0300

    race

--- race.js
+++ race.js
@@ -1,54 +1,42 @@
 function race(promises) {
     return new Promise((resolve, reject) => {
-      for (let i = 0; i < promises.length; i++) {
-        let p = promises[i];
-        if (p instanceof Promise) {
-          p.then(resolve).catch(reject);
-        } else {
-          resolve(p);
+        for (let i = 0; i < promises.length; i++) {
+            let p = promises[i];
+            if (p instanceof Promise) {
+                p.then(resolve).catch(reject);
+            } else {
+                resolve(p);
+            }
         }
-      }
     });
 }
-
-
 
 function some(promises, count) {
-    if (count === 0) {
-      return new Promise((resolve) => resolve([]));
+    if (count === 0 || promises.length === 0) {
+        return Promise.resolve([]);
     }
-  
-    if (promises.length === 0) {
-      return new Promise((resolve) => resolve([]));
-    }
-  
+
     return new Promise((resolve, reject) => {
-      const resolvedValues = new Array(count);
-      let resolvedCount = 0;
-  
-      for (let i = 0; i < promises.length; i++) {
-        let p = promises[i];
-  
-        if (p instanceof Promise) {
-          p.then((value) => {
-            if (resolvedCount < count) {
-              resolvedValues[resolvedCount] = value;
-              resolvedCount++;
-            }
-            if (resolvedCount === count) {
-              resolve(resolvedValues);
-            }
-          }).catch(reject);
-        } else {
-          if (resolvedCount < count) {
-            resolvedValues[resolvedCount] = p;
-            resolvedCount++;
-          }
-          if (resolvedCount === count) {
-            resolve(resolvedValues);
-          }
-        }
-      }
+        const results = new Array(promises.length);
+        let resolvedCount = 0;
+        let settled = false;
+
+        promises.forEach((p, index) => {
+            Promise.resolve(p)
+                .then(value => {
+                    if (settled) return;
+                    results[index] = value;
+                    resolvedCount++;
+
+                    if (resolvedCount === count) {
+                        settled = true;
+                        resolve(results.slice(0, promises.length).filter(v => v !== undefined));
+                    }
+                })
+                .catch(error => {
+                    if (settled) return;
+                    reject(error);
+                });
+        });
     });
 }
-  
